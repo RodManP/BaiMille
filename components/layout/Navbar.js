d4f66b89b320cb6a54daccc7e1969c29bda326d5
Manuel <manuel@empacotecnic.com>
2022-07-27 20:05:18 -0500

    improve Navbar

--- components/layout/Navbar.js
+++ components/layout/Navbar.js
@@ -1,27 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
+import NavItem from "./NavItem";
 import Image from "next/image";
-import styles from "../../styles/navbar.module.css";
+// import styles from "../../styles/navbar.module.css";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { GrClose } from "react-icons/gr";
+import { MdClose } from "react-icons/md";
 
-export default function navbar() {
+const MENU_LIST = [
+  { text: "home", href: "/" },
+  { text: "reservations", href: "/reservations" },
+  { text: "contact", href: "/contact" },
+  { text: "about", href: "/about" },
+];
+
+const Navbar = () => {
+  const [navActive, setNavActive] = useState(false);
+  const [activeIdx, setActiveIdx] = useState(0);
+
+
   return (
-    <nav className={styles.navbar}>
-      <Link href="/">
-        <Image src="/vercel.svg" alt="Icon" width="100px" height="50px" />
-      </Link>
+    <header>
+      <nav className={`nav`}>
+        <Link href={"/"}>
+          <a onClick={() =>setActiveIdx(0)}>
+            <h1 className="logo">Guest House</h1>
+          </a>
+        </Link>
+        <div onClick={() => setNavActive(!navActive)} className="nav__menu-bar">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
 
-      <Link href="/">
-        <a className={styles.active}>Home</a>
-      </Link>
-      <Link href="/reservations">
-        <a>Reservations</a>
-      </Link>
-      <Link href="/contact">
-        <a>Contact</a>
-      </Link>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </nav>
-  );
-}
+        <div className={`${navActive ? 'active' : ''} nav__menu-list`}>
+          {MENU_LIST.map((menu, idx) => {
+            return (
+            <div onClick={() => {
+                setActiveIdx(idx);
+                setNavActive(false);
+              }}
+              key={menu.text} >
+                
+              <NavItem active={activeIdx === idx} {...menu} />
+            </div>
+          )})}
+        </div>
+      </nav>
+    </header>
+)}
+
+export default Navbar;
