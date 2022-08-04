4c7db423b777a236e3d82fe61c781a7e3890a9e9
Manuel <manuel@empacotecnic.com>
2022-08-04 11:03:16 -0500

    Home page vwe 1.0

--- components/layout/Navbar.js
+++ components/layout/Navbar.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import Link from "next/link";
 import NavItem from "./NavItem";
-import Image from "next/image";
 // import styles from "../../styles/navbar.module.css";
 import { GiHamburgerMenu } from "react-icons/gi";
 
--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,74 @@
-import styles from "../styles/home.module.css";
 import Layout from "../components/layout/Layout";
+import styles from "../styles/home.module.css";
+import Image from "next/image";
+import Link from "next/link";
+import NavItem from "../components/layout/NavItem";
 
 export default function Home() {
   return (
     <Layout title="Bai Mille Guest House">
-      <main className={styles.main}>
-        <h1>Home</h1>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
-        <div>2</div>
-        <div>2</div>
-        <div>2</div>
-        <div>2</div>
-        <div>2</div>
-        <div>2</div>
-        <div>2</div>
-        <div>2</div>
-        <div>3</div>
-        <div>3</div>
-        <div>3</div>
-        <div>3</div>
-        <div>3</div>
-        <div>3</div>
-        <div>3</div>
-        <div>4</div>
-        <div>4</div>
-        <div>4</div>
-        <div>4</div>
-        <div>4</div>
-        <div>5</div>
-        <div>5</div>
-        <div>5</div>
-        <div>5</div>
-        <div>5</div>
-        <div>6</div>
-        <div>6</div>
-        <div>6</div>
-        <div>6</div>
-        <div>6</div>
-        <h1>end of home</h1>
+      <main className={styles.body}>
+        <div className={styles.container}>
+          <div className={styles.card_section}>
+            <div className={styles.card}>
+              <Image
+                className={styles.image}
+                src="/bansko.jpg"
+                alt="Bansko"
+                width={350}
+                height={500}
+              />
+              <div className={styles.intro}>
+                <h1 className={styles.title}>Bansko</h1>
+                <p className={styles.subtitle}>
+                  Bulgaria Ski Resort <br></br> Lift is 10 min away walking
+                </p>
+              </div>
+            </div>
+            <div className={styles.card}>
+              <Image
+                className={styles.image}
+                src="/resizedkristin.jpg"
+                alt="Guest House"
+                width={350}
+                height={500}
+              />
+              <div className={styles.intro}>
+                <h1 className={styles.title}>Bai Mile</h1>
+                <p className={styles.subtitle}>Guest House</p>
+              </div>
+            </div>
+            <div className={styles.card}>
+              <Image
+                className={styles.image}
+                src="/ivan-ivanov3.jpg"
+                alt="Downtown"
+                width={350}
+                height={500}
+              />
+              <div className={styles.intro}>
+                <h1 className={styles.title}>Ivan Ivanov</h1>
+                <div>
+                  <p className={styles.subtitle}>
+                    Downtown is 10min away walking
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.text_section}>
+            <div className={styles.name}>Guest House</div>
+            <div className={styles.description}>
+              A comfortable Stay At An Affordable Price
+            </div>
+            <div className={styles.centerbtn}>
+              <button className={styles.btn}>
+                <NavItem text="Book Now" href="/contact" />
+              </button>
+            </div>
+          </div>
+        </div>
       </main>
     </Layout>
   );
